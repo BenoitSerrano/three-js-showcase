29c5159d8166e977c1dd18ed4bee8e9e648cedbf
BenoitSerrano <benoit.serrano10@gmail.com>
2023-03-10 18:16:22 +0100

    example-2

--- src/script.js
+++ src/script.js
@@ -1,42 +1,24 @@
 import * as THREE from 'three'
-// import gsap from "gsap"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import {FontLoader} from 'three/examples/jsm/loaders/FontLoader'
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 import * as dat from 'lil-gui'
 
-const parameters = {
-    color: 0xff0000
-}
-const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-const canvas = document.querySelector('canvas.webgl')
-const group = new THREE.Group()
-
-const normalMaterial = new THREE.MeshNormalMaterial()
-const material = new THREE.MeshToonMaterial()
-normalMaterial.flatShading = true
-const geometry1 = new THREE.TorusGeometry(0.3, 0.2, 16, 32)
-const geometry2 = new THREE.SphereGeometry(1, 32,32)
-const geometry3 = new THREE.PlaneGeometry(1, 1)
-const torus = new THREE.Mesh(geometry1, material)
-torus.position.x = -2
-const sphere = new THREE.Mesh(geometry2, material)
-sphere.position.x = 2
-const plane = new THREE.Mesh(geometry3, material)
-group.add(torus)
-group.add(sphere)
-group.add(plane)
-// scene.add(group)
 
 /**
  * Lights
  */
- const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
- scene.add(ambientLight)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
 
 const pointLight = new THREE.PointLight(0xffffff, 0.5)
 pointLight.position.x = 2
@@ -45,106 +27,111 @@ pointLight.position.z = 4
 scene.add(pointLight)
 
 /**
- * Fonts
+ * Objects
  */
- const fontLoader = new FontLoader()
+// Material
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.4
 
- fontLoader.load(
-     '/fonts/helvetiker_regular.typeface.json',
-     (font) =>
-     {
-         const textGeometry = new TextGeometry(
-             'Hello Three.js',
-             {
-                font: font,
-                size: 0.5,
-                height: 0.2,
-                curveSegments: 12,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 5
-             }
-         )
-         textGeometry.computeBoundingBox()
-         textGeometry.translate(
-            - textGeometry.boundingBox.max.x * 0.5,
-            - textGeometry.boundingBox.max.y * 0.5,
-            - textGeometry.boundingBox.max.z * 0.5
-        )
-                 const textMaterial = new THREE.MeshBasicMaterial({map: matcapTexture})
-         const text = new THREE.Mesh(textGeometry, textMaterial)
-         scene.add(text)
-     }
- )
+// Objects
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+)
+sphere.position.x = - 1.5
 
-// Sizes
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.75, 0.75, 0.75),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
+    material
+)
+torus.position.x = 1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    material
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.65
+
+scene.add(sphere, cube, torus, plane)
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
-const aspectRatio = sizes.width / sizes.height
 
-// Clock
-const clock = new THREE.Clock()
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, aspectRatio, 1, 1000)
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-camera.position.z = 5
-camera.lookAt(group.position)
-scene.add(camera)
-
-
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () =>
+{
+    // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
+
+    // Update camera
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
+
+    // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-window.addEventListener('dblclick', () =>
-{
-    if(!document.fullscreenElement)
-    {
-        canvas.requestFullscreen()
-    }
-    else
-    {
-        document.exitFullscreen()
-    }
-})
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
 
-// Renderer
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-    canvas
+    canvas: canvas
 })
-
-
 renderer.setSize(sizes.width, sizes.height)
-const tick = () => {
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
     const elapsedTime = clock.getElapsedTime()
 
-    sphere.rotation.y = 0.2 * elapsedTime
-    plane.rotation.y = 0.2 * elapsedTime
-    torus.rotation.y = 0.2 * elapsedTime
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    cube.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
 
-    sphere.rotation.x = 0.25 * elapsedTime
-    plane.rotation.x = 0.25 * elapsedTime
-    torus.rotation.x = 0.25 * elapsedTime
+    sphere.rotation.x = 0.15 * elapsedTime
+    cube.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
 
+    // Update controls
     controls.update()
+
+    // Render
     renderer.render(scene, camera)
 
+    // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
 
 tick()
-
-
-
